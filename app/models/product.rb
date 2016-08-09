09bb01d9d6b50b86ddf2338b831c4f209704d004
JohannaOssa <johannaossa10@gmail.com>
2016-08-09 14:08:32 -0500

    modifique tamaño de imagenes en controlador y vistas de Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   validates :nombre, presence: true, uniqueness: true
   validates :descripcion, :tipo_producto, :valor_unitario, presence: true
 
-  has_attached_file :image, styles: {medium: "1280x720", thumb: "480×270"}
+  has_attached_file :image, styles: {medium: "1280x720", thumb: "50×50", mini:"100x100"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
