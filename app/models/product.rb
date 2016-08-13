19826b1b3377ce9155146dfdfebf5d1143e0269e
JohannaOssa <johannaossa10@gmail.com>
2016-08-13 13:25:51 -0500

    modifique relaciones entre los modelos sale y product para relacionar con modelo sale_product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
-  has_and_belongs_to_many :sales
+  has_many :sale_products
+  has_many :sales, through: :sale_products
 
   validates :nombre, presence: true, uniqueness: true
   validates :descripcion, :tipo_producto, :valor_unitario, :stock, presence: true
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,16 +1,18 @@
 class Sale < ActiveRecord::Base
   belongs_to :user
-  has_and_belongs_to_many :products
+  has_many :sale_products
+  has_many :products, through: :sale_products
 
   after_create :save_products
 
-  # Custom Setter:Metodo que me permite asignar un valor al atrinbuto de un objeto, en estecaso el arrglo products
+  # Custom Setter:Metodo que me permite asignar un valor al atributo de un objeto, en este caso el arreglo products
   def products=(value)
     @products = value
   end
 
-  # def save_products
-  #   @products.each do [product_id]
-  #
-  # end
+  def save_products
+    @products.each do |product_id|
+      SaleProduct.create(product_id:product_id, sale_id: self.id)
+    end
+  end
 end
